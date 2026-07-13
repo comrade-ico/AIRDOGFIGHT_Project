56d5a13d5bc55731cc82dc787bf4bb751a3d9041
SpadeKrieg <87633665+comrade-ico@users.noreply.github.com>
2026-07-13 16:41:37 +0800

    gitignore fix

--- AIRDOGFIGHT/AIRDOGFIGHT.cpp
+++ AIRDOGFIGHT/AIRDOGFIGHT.cpp
@@ -8,6 +8,6 @@ int main()
 	std::cout << "嘿！欢迎打开AIRDOGFIGHT——一款空战卡牌游戏！" << std::endl;
 	Sleep(500);
 	std::cout << "第一步是选择您的战机！" << std::endl;
-	std::ifstream fighterInfo;
+	std::ifstream fighterInfo("Fighters.txt");
 
 }
