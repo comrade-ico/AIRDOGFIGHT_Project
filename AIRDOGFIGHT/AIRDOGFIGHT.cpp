0a99cac8900f55aaa2cb35f1f30c9333e874c450
SpadeKrieg <87633665+comrade-ico@users.noreply.github.com>
2026-07-13 16:18:15 +0800

    Initial commit

---
+++ AIRDOGFIGHT/AIRDOGFIGHT.cpp
@@ -0,0 +1,12 @@
+﻿#include <fstream>
+#include<Windows.h>
+#include <iostream>
+//卡牌！？
+
+int main()
+{
+	std::cout << "嘿！欢迎打开AIRDOGFIGHT——一款空战卡牌游戏！" << std::endl;
+	Sleep(500);
+	std::cout << "第一步是选择您的战机！" << std::endl;
+	std::ifstream fighterInfo;
+}
